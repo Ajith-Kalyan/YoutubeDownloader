54fc0129b3f263938ec16e114c64117bd12506ec
Kalyan <47467548+Ajith-Kalyan@users.noreply.github.com>
2024-12-29 20:59:49 +0000

    Modified for web deployment

--- server.py
+++ server.py
@@ -85,4 +85,5 @@ def download():
         return jsonify({"error": f"Unexpected error: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
